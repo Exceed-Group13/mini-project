a77611524e97c5dc0d8eb30f9ef1d8df18c79fdb
saranrak <saranrak.d@ku.th>
2023-02-08 13:49:38 +0700

    aa

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ import os
 import urllib
 load_dotenv('.env')
 
-user = os.getenv('username')
+user = os.getenv('user')
 password = urllib.parse.quote(os.getenv('password'))
 client = MongoClient(f"mongodb://{user}:{password}@mongo.exceed19.online:8443/?authMechanism=DEFAULT")
+
+print(user, password)
 
 db = client["exceed13"] #use database name
 collection = db['miniproject'] # db.collection_name
@@ -18,29 +20,38 @@ class Light(BaseModel):
     state: bool
     room: int
     mode: bool
-    room_inten: int
     light: int
     
 data = [{
     "state": False,
     "room": 1,
     "mode": 0,
-    "room_inten": 2600,
     "light": 0
 },{
     "state": False,
     "room": 2,
     "mode": 0,
-    "room_inten": 4000,
     "light": 0
 },{
     "state": False,
     "room": 3,
     "mode": 1,
-    "room_inten": 3000,
     "light": 0
 }]
 
 app = FastAPI()
-collection.delete_many({})
-a = collection.insert_many(data)
+#collection.delete_many({})
+# a = collection.insert_many(data)
+
+@app.patch("/dim")
+def dim_light(detail: Light):
+    room = collection.find_one({"room": detail.room})
+    if detail.mode==0:
+        room1 = collection.find_one_and_update({"room": detail.room},{'$set': {'light': detail.light}})
+        if detail.light==0:
+            light_set = collection.find_one_and_update({"room": detail.room},{'$set': {'state': False}})        
+    ans = {"state": detail.state,
+            "room": detail.room,
+            "mode": detail.mode,
+            "light": detail.light}
+    return ans
