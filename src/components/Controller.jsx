066f1e8963fe50a702bd578a8f55e2d7a5428eeb
natekrth <natekrth@gmail.com>
2023-02-08 15:01:52 +0700

    add event listener and fetch every 1000 ms

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import React , { useEffect } from 'react'
 // import { Link } from 'react-router-dom'
 // import '/App.css'
 
-const Controller = (pros) => {
-console.log(pros)
+const Controller = (props) => {
+    
+    console.log(props)
+
+    useEffect(()=>{
+        setInterval(() => {
+            console.log("Tick!")
+        },1000)
+    },[])
+
+
+    function manageCheckBox(led,ev) {
+        if (ev.target.checked) {
+            console.log(ev.target.value)
+        }else {
+            console.log(led,"close")
+        }
+
+    }
 	return (
 		<div className="component">
             <div className="room1">
                 <div className="Bedroom">
                     <br/>
-                    <h1 >{pros.Name}</h1>
+                    <h1 >{props.Name}</h1>
                     <br/>
                     <h2>OFF</h2>
                     <br/>
                     <h2>ON</h2>
                     <div className="form-check form-switch">
-                        <input className="form-check-input" type="checkbox" />
+                        <input className="form-check-input" type="checkbox" value={`${props.led} open`} onChange={(ev) => manageCheckBox(props.led, ev)}/>
                     </div> 
                     <div className="range">
                         <input type="range" class="form-range" id="customRange1"></input>
