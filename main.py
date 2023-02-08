7609fb449ca6f9aa04508da0ffd0c85f2cd3272b
saranrak <saranrak.d@ku.th>
2023-02-08 13:57:04 +0700

    add dim function

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ collection = db['miniproject'] # db.collection_name
 class Light(BaseModel):
     state: bool
     room: int
-    mode: bool
+    mode: str
     light: int
     
 class Command(BaseModel):
@@ -39,34 +39,18 @@ data = [{
 },{
     "state": False,
     "room": 3,
-<<<<<<< HEAD
     "mode": 1,
-=======
     "mode": 0,
->>>>>>> aac5dc3743d3b30b3f5fa433f88f4a206706d294
     "light": 0
 }]
 
+# collection.delete_many({})
+# a = collection.insert_many(data)
+
 app = FastAPI()
-<<<<<<< HEAD
 #collection.delete_many({})
 # a = collection.insert_many(data)
 
-@app.patch("/dim")
-def dim_light(detail: Light):
-    room = collection.find_one({"room": detail.room})
-    if detail.mode==0:
-        room1 = collection.find_one_and_update({"room": detail.room},{'$set': {'light': detail.light}})
-        if detail.light==0:
-            light_set = collection.find_one_and_update({"room": detail.room},{'$set': {'state': False}})        
-    ans = {"state": detail.state,
-            "room": detail.room,
-            "mode": detail.mode,
-            "light": detail.light}
-    return ans
-=======
-collection.delete_many({})
-a = collection.insert_many(data)
 
 @app.get("/lights")
 def get_lights():
@@ -87,4 +71,16 @@ def control_light(command: Command):
     light = collection.find_one_and_update({'room': command.room}, 
         {'$set': {'state': command.state}})
     return {'response': "success"}
->>>>>>> aac5dc3743d3b30b3f5fa433f88f4a206706d294
+
+@app.patch("/dim")
+def dim_light(detail: Light):
+    room = collection.find_one({"room": detail.room})
+    if detail.mode==0:
+        room1 = collection.find_one_and_update({"room": detail.room},{'$set': {'light': detail.light}})
+        if detail.light==0:
+            light_set = collection.find_one_and_update({"room": detail.room},{'$set': {'state': False}})        
+    ans = {"state": detail.state,
+            "room": detail.room,
+            "mode": detail.mode,
+            "light": detail.light}
+    return ans
