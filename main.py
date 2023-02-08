3f61c3b27dd96e3f63186f9078ec9701a65c6fa8
saranrak <saranrak.d@ku.th>
2023-02-08 14:14:03 +0700

    resolve conflict

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ import os
 import urllib
 load_dotenv('.env')
 
-user = os.getenv('user')
+user = os.getenv('username')
 password = urllib.parse.quote(os.getenv('password'))
 client = MongoClient(f"mongodb://{user}:{password}@mongo.exceed19.online:8443/?authMechanism=DEFAULT")
-
-print(user, password)
 
 db = client["exceed13"] #use database name
 collection = db['miniproject'] # db.collection_name
@@ -39,22 +37,13 @@ data = [{
 },{
     "state": False,
     "room": 3,
-<<<<<<< HEAD
-    "mode": 1,
-    "mode": 0,
-=======
     "mode": "manual",
->>>>>>> 621eed22e06cf78e8afd7d4319953d51d0c84554
     "light": 0
 }]
 
-# collection.delete_many({})
-# a = collection.insert_many(data)
-
 app = FastAPI()
-#collection.delete_many({})
-# a = collection.insert_many(data)
-
+collection.delete_many({})
+a = collection.insert_many(data)
 
 @app.get("/lights")
 def get_lights():
@@ -72,8 +61,13 @@ def control_light(command: Command):
         {'$set': {'state': command.state}})
     return {'response': "success"}
 
-<<<<<<< HEAD
-@app.patch("/dim")
+@app.patch("/lights/mode/{mode}")
+def change_mode(mode):
+    """Change lights mode."""
+    light = collection.update_many({}, {'$set': {'mode': mode}})
+    return {'response': "mode changed"}
+
+    @app.patch("/dim")
 def dim_light(detail: Light):
     room = collection.find_one({"room": detail.room})
     if detail.mode==0:
@@ -85,10 +79,3 @@ def dim_light(detail: Light):
             "mode": detail.mode,
             "light": detail.light}
     return ans
-=======
-@app.patch("/lights/mode/{mode}")
-def change_mode(mode):
-    """Change lights mode."""
-    light = collection.update_many({}, {'$set': {'mode': mode}})
-    return {'response': "mode changed"}
->>>>>>> 621eed22e06cf78e8afd7d4319953d51d0c84554
