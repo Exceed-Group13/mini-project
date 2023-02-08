4860e72366a0152786e06548b2b90d58d559710e
natekrth <natekrth@gmail.com>
2023-02-08 15:37:01 +0700

    fix missing line

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -5,7 +5,6 @@ import React , { useEffect } from 'react'
 const Controller = (props) => {
     
     console.log(props)
-
     useEffect(()=>{
         setInterval(() => {
             console.log("Tick!")
@@ -31,10 +30,10 @@ const Controller = (props) => {
                     <div className="form-switch">
                     <h2 className='off'>OFF</h2>
                     <div className='box'>
-                        <input className="form-check-input" type="checkbox" />
+                        <input className="form-check-input" type="checkbox" value={`${props.led} open`} onChange={(ev) => manageCheckBox(props.led, ev)}/>
                     </div>
                     <h2 className='on'>ON</h2>
-                    </div> 
+                    </div>
                     <div className="range">
                         <input type="range" class="form-range" id="customRange1"></input>
                     </div>
