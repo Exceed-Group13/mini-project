33037b383a04d70dcc616000509a42e4a4e99733
natekrth <natekrth@gmail.com>
2023-02-08 22:53:35 +0700

    update ui and link completed to backend

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -1,80 +1,105 @@
-import React , { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import { MaterialUISwitch } from "./MaterialUISwitch";
+import Slider, { SliderThumb } from "@mui/material/Slider";
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+import Box from "@mui/material/Box";
 // import { Link } from 'react-router-dom'
 // import '/App.css'
 
 const Controller = (props) => {
-    const [controller, setController] = useState(false)
+  const [controller, setController] = useState(false);
 
-    function manageSwitch(room, state) {
-        const requestOptions = {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            "state": !state,
-            "room": room
-        })
+  function manageSwitch(room, state) {
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        state: !state,
+        room: room,
+      }),
     };
-    fetch('https://ecourse.cpe.ku.ac.th/exceed13/light/control', requestOptions)
-        .then(response => response.json())
-        .then(data => this.setState({ postId: data.id }));
-    }
 
-    function manageDim(room, dim) {
-        const requestOptions = {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            "light": dim,
-            "room": room
-        })
+    fetch("https://ecourse.cpe.ku.ac.th/exceed13/light/control", requestOptions)
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+  }
+
+  function manageDim(room, dim) {
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        light: dim,
+        room: room,
+      }),
     };
-    fetch('https://ecourse.cpe.ku.ac.th/exceed13/dim', requestOptions)
-        .then(response => response.json())
-        .then(data => this.setState({ postId: data.id }));
+    fetch("https://ecourse.cpe.ku.ac.th/exceed13/dim", requestOptions)
+      .then((response) => response.json())
+      .then((data) => setController({ postId: data.id }));
+  }
+
+  useEffect(() => {
+    if (props.data) {
+      setController(props.data.state);
+      console.log("data", props.data.state);
     }
+  }, [props.data]);
 
-    useEffect(() => {
-        if (props.data) {
-            setController(props.data.state)
-            console.log('data',props.data.state)
-        }
-    } , [props.data])
+  useEffect(() => {
+    console.log(controller);
+  }, [controller]);
 
-    useEffect(() => {
-        console.log(controller)
-    },[controller])
-
-    function manageCheckBox(led,ev) {
-        if (ev.target.checked) {
-            // console.log(ev.target.value)
-            return true
-        }else {
-            // console.log(led,"close")
-            return false
-        }
-
+  function manageCheckBox(led, ev) {
+    if (ev.target.checked) {
+      // console.log(ev.target.value)
+      return true;
+    } else {
+      // console.log(led,"close")
+      return false;
     }
-	return (
-		<div className="component">
-            <div className="room1">
-                <div className="Bedroom">
-                    <br/>
-                    <h1 >{props.Name}</h1>
-                    <br/>
-                    <div className="form-switch">
-                    <h2 className='off'>OFF</h2>
-                    <div className='box'>
-                        <input className="form-check-input" type="checkbox" checked={controller} onChange={(e) => manageSwitch(props.data.room, props.data.state)}/>
-                    </div>
-                    <h2 className='on'>ON</h2>
-                    </div>
-                    <div className="range">
-                        <input type="range" className="form-range" id="customRange1" disabled={!controller} min="0" max="255" onChange={(e) => manageDim(props.data.room, e.target.value)}></input>
-                    </div>
-                </div> 
-            </div>
+  }
+  console.log(props);
+  return (
+    <div className="component">
+      <div className="room1">
+        <div className="Bedroom">
+          <br />
+          <h1>{props.Name}</h1>
+          <br />
+          <br />
+
+          <br />
+          <div>
+            <span style={{ fontSize: "40px" }}>OFF</span>
+            <MaterialUISwitch
+              checked={controller}
+              onChange={() => {
+                manageSwitch(props.data.room, controller);
+              }}
+            />
+            <span style={{ fontSize: "40px" }}>ON</span>
+          </div>
+          <div>
+            <Box sx={{ width: 320 }}>
+              <Box sx={{ m: 3 }} />
+              <Typography gutterBottom>Tooltip value label</Typography>
+              <Slider
+                valueLabelDisplay="auto"
+                onChange={(e) => {
+                  manageDim(props.data.room, e.target.value);
+                }}
+
+                aria-label="custom thumb label"
+                defaultValue={20}
+              />
+            </Box>
+          </div>
         </div>
-	)
-}
+      </div>
+    </div>
+  );
+};
 
-export default Controller
+export default Controller;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
 const Home = () => {
+  const [data, setData] = useState(undefined)
+    function manageMode(mode) {
+        const requestOptions = {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            mode: mode
+          }),
+        };
+        fetch("https://ecourse.cpe.ku.ac.th/exceed13/lights/mode", requestOptions)
+          .then((response) => response.json())
+          .then((data) => setData({ postId: data.id }));
+      }
     return (
         <div >
         <h1>Light Control</h1>
         <div className="divButton">
             <Link to="/auto">
-                <Button className="buttonCtrl">Automatic</Button>
+                <Button className="buttonCtrl" onClick={() => {
+                manageMode("auto");
+              }}>Automatic</Button>
             </Link>
             <Link to="/manual">
-                <Button className="buttonCtrl">Manual</Button>
+                <Button className="buttonCtrl" onClick={() => {
+                manageMode("manual");
+              }} >Manual</Button>
             </Link>
         </div>
         </div>
