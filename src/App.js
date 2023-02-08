711a0305c4e1cb05e2106986a0a136db49b8d287
Fortran <kawintida.pi@ku.th>
2023-02-08 11:44:47 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>
+          test
+        </a>
+        <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+
+const Home = () => {
+    return (
+        <div>
+        <h1>Light Control</h1>
+        <div>
+            <Button>automatic</Button>
+            <Button>Manual</Button>
+        </div>
+        </div>
+    );
+}
+export default Home
